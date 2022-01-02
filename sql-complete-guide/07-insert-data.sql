cfb0023871e00882d0a6b190534c93e4845a0e53
Dan Possehl <danielbpossehl@gmail.com>
2022-01-02 11:45:32 -0700

    added user Dan Possehl

---
+++ sql-complete-guide/07-insert-data.sql
@@ -0,0 +1,11 @@
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+-- VALUEs ('Big Boi Toys', '4356 dude st, Michigan', 323.33, TRUE);
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+-- VALUEs ('Mosters comp', '1555 n clarkson st', 3.4, False);
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+-- VALUEs ('Disney INC', '3263 Disney ST, Orlando', 522, TRUE);
+
+INSERT INTO conversation (user_name, employer_name, message, date_sent)
+VALUES ("Dan Possehl", 'Big Boi Toys', "Hi I like Learning", '2021-10-15 16.32:20')
