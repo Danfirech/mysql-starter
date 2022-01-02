7279951150593fb50043c1c17d75fcbcda0f3c81
Dan Possehl <danielbpossehl@gmail.com>
2022-01-02 09:41:03 -0700

    added data

--- sql-complete-guide/02-create-users-table.sql
+++ sql-complete-guide/02-create-users-table.sql
@@ -10,6 +10,4 @@ full_name VARCHAR(200),
 yearly_salary INT,
 current_status employment_status
 -- current_status ENUM('employed', 'self-employed', 'unemployed') -- MYSQL SYNTAX
-
-
 );
---
+++ sql-complete-guide/03-insert-users-data.sql
@@ -0,0 +1,9 @@
+-- INSERT INTO users (yearly_salary, full_name, current_status) 
+-- VALUES (19000, 'Max Schwarz', 'self-employed');
+
+-- INSERT INTO users (yearly_salary, full_name, current_status) 
+-- VALUES (0, 'Nat Scott', 'unemployed');
+
+INSERT INTO users (yearly_salary, full_name, current_status) 
+VALUES (50000, 'Dan Possehl', 'employed');
+
